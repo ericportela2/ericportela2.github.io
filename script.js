cf839d111a0e24ba86bd71370abbe43eefe302a0
EricPortela <ericq.portela@gmail.com>
2023-05-13 15:46:09 +0200

    Added animation to search

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ function searchPackages() {
     // loop through the h1 elements and hide/show the list item based on the search value
     for (let title of packageTitles) {
         let listItem = title.parentElement.parentElement.parentElement.parentElement;
+        let packageCard = title.parentElement.parentElement;
+        
         if (title.innerHTML.toLowerCase().indexOf(searchValue) > -1) {
-            listItem.style.display = "";
+            listItem.classList.remove('hidden');
+            packageCard.classList.add('visible');
         } else {
-            listItem.style.display = "none";
+            listItem.classList.add('hidden');
+            packageCard.classList.remove('visible');
         }
     }
 }
 
+// function searchPackages() {
+//     // get the search input value
+//     let searchValue = document.getElementById('search-bar').value.toLowerCase();
 
+//     // get all the h1 elements within the list items
+//     let packageTitles = document.querySelectorAll('#packages-ul li h1');
+
+//     // loop through the h1 elements and hide/show the list item based on the search value
+//     for (let title of packageTitles) {
+//         let listItem = title.parentElement.parentElement.parentElement.parentElement;
+//         if (title.innerHTML.toLowerCase().indexOf(searchValue) > -1) {
+//             listItem.style.display = "";
+//         } else {
+//             listItem.style.display = "none";
+//         }
+//     }
+// }
+  
